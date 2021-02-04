56a34879fcde31f5ec974058ad7b73f3ed755563
Garun Vagidov <garun@garunski.com>
2021-02-04 08:03:42 -0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { App1Module } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
